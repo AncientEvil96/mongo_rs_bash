d6e1cd035d9cd9cdca81af87103c332e85b44867
AncientEvil96 <bersek123@gmail.com>
2021-11-03 14:29:38 +0300

    create py_test

--- test.py
+++ test.py
@@ -1,3 +1,21 @@
-import pymongo
+from pymongo import MongoClient
 
 host = 'mongo_rs_1,mongo_rs_2,mongo_rs_3'
+tlsCertificateKeyFile = '192.168.1.116'
+tlsCAFile = 'mongoCA.pem'
+username = 'root'
+password = 'root'
+
+with MongoClient(
+    host,
+    username=username,
+    password=password,
+    replicaSet = True,
+    tls = True,
+    tlsCertificateKeyFile=tlsCertificateKeyFile,
+    tlsCAFile=tlsCAFile
+) as conn:
+    db = conn['test']
+    connection = db['test']
+
+    connection
