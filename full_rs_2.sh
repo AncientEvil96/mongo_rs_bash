1d4377f190f07b123a9fc11de933728ae63ebd89
AncientEvil96 <bersek123@gmail.com>
2021-11-10 08:31:52 +0300

    asd

--- full_rs_2.sh
+++ full_rs_2.sh
@@ -1,8 +1,4 @@
 #!/bin/bash
 
-mkdir -m 777 -p docker/ssl
-
-cp mongoCA.pem docker/ssl
-
 bash new_srv_mongo.sh -rm -new_srv mongo_rs_3 -rs_add mongo_rs_1 -ip 172.16.238.4
-bash new_srv_mongo.sh -rm -rs_arb -new_srv mongo_rs_arb_2 -rs_add mongo_rs_1 -ip 172.16.238.6ll
+bash new_srv_mongo.sh -rm -rs_arb -new_srv mongo_rs_arb_2 -rs_add mongo_rs_1 -ip 172.16.238.6
--- test.py
+++ test.py
@@ -1,21 +1,27 @@
 from pymongo import MongoClient
 
-host = 'mongo_rs_1,mongo_rs_2,mongo_rs_3'
-tlsCertificateKeyFile = '192.168.1.116'
+host = '192.168.2.172:27017'
+tlsCertificateKeyFile = '192.168.1.166.pem'
 tlsCAFile = 'mongoCA.pem'
 username = 'root'
 password = 'root'
 
 with MongoClient(
-    host,
-    username=username,
-    password=password,
-    replicaSet = True,
-    tls = True,
-    tlsCertificateKeyFile=tlsCertificateKeyFile,
-    tlsCAFile=tlsCAFile
+        host,
+        username=username,
+        password=password,
+        replicaSet='rs0',
+        tls=True,
+        tlsCertificateKeyFile=tlsCertificateKeyFile,
+        tlsCAFile=tlsCAFile
 ) as conn:
     db = conn['test']
     connection = db['test']
 
-    connection
+    for i in range(1, 1000000):
+        if i % 2 == 0:
+            connection.insert_one({'name': 'Tom', 'type': 'cat'})
+            print(connection.count_documents({'name': 'Tom'}))
+        else:
+            connection.insert_one({'name': 'Jerry', 'type': 'mouse'})
+            print(connection.count_documents({'name': 'Jerry'}))
